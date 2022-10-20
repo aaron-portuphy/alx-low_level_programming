cd1bbd07baf287153b47bfed7f653ead0de89182
aaron-portuphy <aaronportuphy@gmail.com>
2022-10-20 16:57:01 -0700

    updated

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,15 +10,15 @@ void more_numbers(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-			for (num = 0; num <= 14; num++)
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
 			{
-				if (num > 9)
-				{
-					_putchar((num / 10) + '0');
-				}
-				_putchar((num % 10) + '0');
+				_putchar((num / 10) + '0')
 			}
+			_putchar((num % 10) + '0');
+		}
 
-			_putchar('\n')
+		_putchar('\n');
 	}
 }
