d54d1726ed7d811477fde9e6988fae6bd7b2c818
aaron-portuphy <aaronportuphy@gmail.com>
2022-10-20 17:08:01 -0700

    updated last

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,11 +14,10 @@ void more_numbers(void)
 		{
 			if (num > 9)
 			{
-				_putchar((num / 10) + '0')
+				_putchar((num / 10) + '0');
 			}
 			_putchar((num % 10) + '0');
 		}
-
 		_putchar('\n');
 	}
 }
