e48a5d6ae3677152b7a3959d1cfd4a49deca1c7e
aaron-portuphy <aaronportuphy@gmail.com>
2022-10-27 14:29:50 -0700

    updated

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -22,8 +22,8 @@ char *cap_string(char *str)
 				str[i - 1] == '"' ||
 				str[i - 1] == '(' ||
 				str[i - 1] == ')' ||
-				str[i - 1] == '}' ||
 				str[i - 1] == '{' ||
+				str[i - 1] == '}' ||
 				i == 0)
 			str[i] -= 32;
 
