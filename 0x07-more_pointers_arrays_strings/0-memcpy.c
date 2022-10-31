3b2bb68a666280dd020c331e52287af89cdddf5d
aaron-portuphy <aaronportuphy@gmail.com>
2022-10-31 00:28:54 -0700

    Updated Task 0

--- 0x07-more_pointers_arrays_strings/0-memcpy.c
+++ 0x07-more_pointers_arrays_strings/0-memcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _memset -fills memory with a constant byte
+ * _memset - fills memory with a constant byte
  * @s: memory area
  * @b: the byte
  * @n: # of bytes to fill
