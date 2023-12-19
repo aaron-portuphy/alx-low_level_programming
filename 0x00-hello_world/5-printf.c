6d986e24ebc5923e5993ac454a5383955b908c9a
Aaron Portuphy <aaronportuphy@gmail.com>
2023-12-18 21:40:15 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,9 +5,12 @@
  * main - prints "with proper grammar, but the outcome is
  * a piece of art," followed by a new lline
  * Return: Always 0.
+ *
  */
 
 int main(void)
+
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	retur(0);
