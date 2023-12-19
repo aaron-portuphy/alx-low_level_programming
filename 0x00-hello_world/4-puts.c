1c068625c04a11ebdab790d9f3dcd6f63d93f86d
Aaron Portuphy <aaronportuphy@gmail.com>
2023-12-18 21:15:15 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio>
+
+
+/**
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by a new line."
+ * returnn 0.
+ * /
+
+ int main(void)
+
+{
+	puts("\"Programming is like buildinnng a multilingual puzzle");
+
+	return(0);
+}	
