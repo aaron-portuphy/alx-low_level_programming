3ec4daf646a52cf8d2a2943b62530a36145c1b9e
Aaron Portuphy <aaronportuphy@gmail.com>
2023-12-19 21:10:04 -0800

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Make alphabet
+ * Return: vvoid
+ */
+
+void print_aphabetx10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i <= 0)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+			_putchar('\n');
+			i++;
+	}
+}
